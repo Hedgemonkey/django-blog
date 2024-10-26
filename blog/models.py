3a3136d0bee0ae3de01da785f96a59d3a3a98752
Hedgemonkey <hedgemonkey@gmail.com>
2024-10-26 15:04:42 +0100

    Add Field to post, Add Comment table, Add Meta and display strings to models

--- blog/models.py
+++ blog/models.py
@@ -11,11 +11,29 @@ STATUS = (
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    updated_on = models.DateTimeField(auto_now=True)
+    excerpt = models.TextField(blank=True)
     content = models.TextField()
+    updated_on = models.DateTimeField(auto_now=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f'Post: {self.title} | Created by {self.author}'
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='commenter')
     body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
 
+    class Meta:
+        ordering = ['-created_on']
 
+    def __str__(self):
+        return f'Comment: {self.body} | Created by {self.author}'                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
