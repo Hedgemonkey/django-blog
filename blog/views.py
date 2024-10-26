04644d842cdd55b4e162ae82bb09bf69063a2fed
Hedgemonkey <hedgemonkey@gmail.com>
2024-10-26 15:53:04 +0100

    Add generic ListView for blog

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,6 @@ from django.views import generic
 from .models import Post
 
 # Create your views here.
-class PostLst(generic.ListView):
-    queryset = Post.objects.all()
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'post_list.html'
