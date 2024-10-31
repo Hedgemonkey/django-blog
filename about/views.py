c5c93421bf35347beba89fe771bfe3d1bc1777b5
Hedgemonkey <hedgemonkey@gmail.com>
2024-10-31 18:20:37 +0000

    Add collaborate request functions

--- about/views.py
+++ about/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.views import generic
+from django.http import Http404
 from .models import About
+from .forms import CollaborateForm
+from django.contrib import messages
 
 
 
@@ -10,6 +12,22 @@ def about(request):
     Select the most recently updated object in :model:`about.About` and display it in the about page.
     """
     about = About.objects.order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
+
     if not about:
         raise Http404("No About object found.")
-    return render(request, "about/about.html", {"about": about},)
+    
+    if request.method == 'POST':
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.success(request, "Collaboration request received! We'll get back to you soon.")
+    
+    return render(
+        request,
+        "about/about.html",
+        {
+            "about": about,
+            "collaborate_form": collaborate_form
+        },
+    )
